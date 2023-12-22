b8f2d8648adab79fe46aefec45b420fb0f190b33
Catsuuuu <154604528+Catsuuuu@users.noreply.github.com>
2023-12-22 14:06:12 +0000

    Done Adding Output

--- HelloWorldApp.java
+++ HelloWorldApp.java
@@ -1,1 +1,27 @@
+/** 
+ * Copied HelloWorld.java
+ * 
+ * The HelloWorldApp class implements an application that
+ * simply prints "Hello World!" to standard output.
+ 
+class HelloWorldApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World!); // Display the string.
+    }
+}
+*/
+
+/** 
+ * Debugged HelloWorld.java with an output Kamusta Mundo!
+ * 
+ * The HelloWorldApp class implements an application that
+ * simply prints "Hello World!" to standard output.
+ */
+class HelloWorldApp {
+    public static void main(String[] args) {
+        System.out.println("Kamusta Mundo!"); // Display the string.
+    }
+}
+
+
 
